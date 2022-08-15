a608e1824669257b9763430427ad3c1c79e02e4c
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-08-16 00:55:34 +0300

    commit tree 07e4503e675ad33df988e08585d9094788253d91

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe 
+    attr_reader :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def properties(brand = "Nike")
+        @brand = brand
+    end
+     def color (color = "red")
+        @color = color
+     end
+     def size (size = 9.5)
+        @size = size
+     end
+     def material(material = "suede")
+        @material = material
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
